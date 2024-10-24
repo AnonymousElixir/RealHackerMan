a35d98e8a13ed4a7b659b218a9b8fe6d0cc59c06
AnonymousElixir <cassidy@cassidycamp.work>
2024-10-24 14:38:48 -0500

    Update example.py

--- example.py
+++ example.py
@@ -1,41 +1,42 @@
-    code = """
-    # Define a function
-    FUNC: my_cool_function(param1, param2)
+from hackermaninterp import RealHackerManInterpreter
+code = """
+# Define a function
+FUNC: my_cool_function(param1, param2)
 
-    # Use the function
-    CALL: my_cool_function("Hello", "World")
+# Use the function
+CALL: my_cool_function("Hello", "World")
 
-    # Variable assignments
-    secret_message = "Hacking is fun!"
-    numbers = [1, 2, 3, 4, 5]
-    data = {"key": "value", "another_key": [10, 20, 30]}
+# Variable assignments
+secret_message = "Hacking is fun!"
+numbers = [1, 2, 3, 4, 5]
+data = {"key": "value", "another_key": [10, 20, 30]}
 
-    # Print statements
-    PRINT: secret_message
-    PRINT: numbers
-    PRINT: data
+# Print statements
+PRINT: secret_message
+PRINT: numbers
+PRINT: data
 
-    # Conditional statements
-    IF: secret_message == "Hacking is fun!"
-    IF: secret_message == "Hacking is fun!" OR numbers[0] == 1
-    IF: secret_message == "Not fun" AND numbers[1] == 2
+# Conditional statements
+IF: secret_message == "Hacking is fun!"
+IF: secret_message == "Hacking is fun!" OR numbers[0] == 1
+IF: secret_message == "Not fun" AND numbers[1] == 2
 
-    # Looping
-    LOOP: 5 times
+# Looping
+LOOP: 5 times
 
-    # Shouting and silence
-    SHOUT: HACKING IS AWESOME!
-    SILENCE: This is a secret message.
+# Shouting and silence
+SHOUT: HACKING IS AWESOME!
+SILENCE: This is a secret message.
 
-    # Random hacker command
-    RANDOM HACKER COMMAND
+# Random hacker command
+RANDOM HACKER COMMAND
 
-    # Sleep
-    SLEEP: 3
+# Sleep
+SLEEP: 3
 
-    # Code Red
-    CODE RED
-    """
+# Code Red
+CODE RED
+"""
 
-    interpreter = RealHackerManInterpreter()
-    interpreter.execute(code)
+interpreter = RealHackerManInterpreter()
+interpreter.execute(code)
